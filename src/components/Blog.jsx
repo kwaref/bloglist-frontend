24a2f6db7f29bf9e761dd7c05d2f0167a7028c3b
Karel R. Varona <kwaref@gmail.com>
2023-12-04 15:06:30 +0000

    feat: 5.7 done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [open, setOpen] = useState(false)
+
+  return <div style={blogStyle}>
+    <p style={{ margin: '0px' }}>
+      {blog.title} {blog.author} <button onClick={() => setOpen(!open)}>{open ? 'hide':'view'}</button>
+    </p>
+    <div style={{ display: open ? 'block' : 'none' }}>
+      <p style={{ margin: '0px' }}>{blog.url}</p>
+      <p style={{ margin: '0px' }}>likes {blog.likes || 0} <button>like</button></p>
+      <p style={{ margin: '0px' }}>{blog.author}</p>
+    </div>
   </div>
-)
+}
+
 
 export default Blog
